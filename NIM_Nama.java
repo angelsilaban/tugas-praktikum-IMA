0ec14d23d4d41ddb2bdaad3a2c1b600eefe5e4fd
angel silaban <magdalenangela20@gmail.com>
2021-09-28 09:33:59 +0700

    first commit

---
+++ NIM_Nama.java
@@ -0,0 +1,8 @@
+public class NIM_Nama {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Angel Silaban");
+		System.out.println("NIM: 6706210052");
+		System.out.println("Kelas: D3IF45-04");
+	}
+}
